241990c4c41f07570d1adff7a28e6213a54ce83d
Monaratuliu05 <monaratuliumona05@gmail.com>
2024-10-26 11:18:52 +0700

    add commit

---
+++ app/src/main/java/id/ac/polbeng/wani/menuexample/MainActivity.kt
@@ -0,0 +1,18 @@
+package id.ac.polbeng.wani.menuexample
+import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import id.ac.polbeng.wani.menuexample.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.myToolbar)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+}
